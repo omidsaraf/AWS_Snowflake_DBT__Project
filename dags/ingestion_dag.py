8a55b30201d5e729e02c05a01aeb2ad53918be21
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2026-01-04 10:55:05 +1100

    Update ingestion_dag.py

--- dags/ingestion_dag.py
+++ dags/ingestion_dag.py
@@ -1,37 +1,69 @@
-## Bronze Layer / Raw Ingestion
-
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.utils.dates import days_ago
+from airflow.providers.amazon.aws.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 default_args = {
     'owner': 'data-engineering',
     'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email': ['data-team@company.com'],
     'email_on_failure': True,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    dag_id='bronze_ingestion_dag',
+    'banking_data_ingestion',
     default_args=default_args,
-    description='Ingest raw banking data into Bronze Layer',
-    schedule_interval='@daily',
-    start_date=days_ago(1),
+    description='Ingest banking data from S3 to Snowflake',
+    schedule_interval='0 2 * * *',  # Daily at 2 AM
     catchup=False,
-    tags=['banking', 'ingestion', 'spark']
+    tags=['banking', 'ingestion'],
 ) as dag:
 
-    bronze_ingestion = KubernetesPodOperator(
-        namespace='default',
-        image='yourdockerhub/spark:latest',
-        cmds=["spark-submit", "--master", "k8s://https://kubernetes.default.svc", "/opt/spark_jobs/bronze_ingestion.py"],
-        name='bronze-ingestion-job',
-        task_id='bronze_ingestion_task',
-        get_logs=True,
-        is_delete_operator_pod=True
+    # Task 1: Spark job to process raw files in S3
+    spark_process_raw = SparkSubmitOperator(
+        task_id='spark_process_raw_data',
+        application='/opt/airflow/spark_jobs/bronze_ingestion.py',
+        conn_id='spark_default',
+        conf={
+            'spark.executor.memory': '4g',
+            'spark.driver.memory': '2g',
+            'spark.executor.cores': '2',
+        },
+        application_args=[
+            '--source-path', 's3://banking-raw/{{ ds }}/',
+            '--target-path', 's3://banking-processed/{{ ds }}/',
+        ],
     )
 
-    bronze_ingestion
+    # Task 2: Load to Snowflake
+    load_customers = S3ToSnowflakeOperator(
+        task_id='load_customers_to_snowflake',
+        snowflake_conn_id='snowflake_default',
+        s3_keys=['s3://banking-processed/{{ ds }}/customers.parquet'],
+        table='RAW.BANKING.CUSTOMERS',
+        schema='BANKING',
+        stage='BANKING_STAGE',
+        file_format='PARQUET_FORMAT',
+    )
+
+    # Task 3: Run data quality checks
+    quality_check = SnowflakeOperator(
+        task_id='run_quality_checks',
+        snowflake_conn_id='snowflake_default',
+        sql="""
+            SELECT 
+                COUNT(*) as record_count,
+                COUNT(DISTINCT customer_id) as unique_customers
+            FROM RAW.BANKING.CUSTOMERS
+            WHERE load_date = '{{ ds }}';
+        """,
+    )
+
+    # Task dependencies
+    spark_process_raw >> load_customers >> quality_check
