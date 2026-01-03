25d6f0af8ca0b781732c0a769bba2e26b8373199
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2026-01-03 18:54:46 +1100

    Update stg_customer.sql

--- dbt/models/staging/stg_customer.sql
+++ dbt/models/staging/stg_customer.sql
@@ -1,18 +1,43 @@
-{{ config(
-    materialized='view'
-) }}
+-- {{ config(
+--     materialized='view'
+-- ) }}
 
-WITH raw AS (
-    SELECT
-        customer_id,
-        first_name,
-        last_name,
-        email,
-        created_at,
-        updated_at
-    FROM {{ source('raw_bronze', 'customer') }}
-)
+-- WITH raw AS (
+--     SELECT
+--         customer_id,
+--         first_name,
+--         last_name,
+--         email,
+--         created_at,
+--         updated_at
+--     FROM {{ source('raw_bronze', 'customer') }}
+-- )
 
-SELECT *
-FROM raw
+-- SELECT *
+-- FROM raw
 ---- Cleans and standardizes raw Bronze data for hubs and satellites.
+-- dbt/models/staging/stg_customer.sql
+{%- set yaml_metadata -%}
+source_model: 'raw_customers'       -- Mapping from your sources.yml
+derived_columns:
+  RECORD_SOURCE: '!S3_CORE_BANKING' -- Constant to identify the origin
+  LOAD_DATETIME: 'CURRENT_TIMESTAMP()'
+hashed_columns:
+  # Business Key Hash for HUB_CUSTOMER
+  CUSTOMER_HK: 'customer_id'
+  
+  # Change tracking hash for SAT_CUSTOMER_DETAILS
+  CUSTOMER_HASHDIFF:
+    is_hashdiff: true
+    columns:
+      - 'first_name'
+      - 'last_name'
+      - 'email'
+{%- endset -%}
+
+{% set metadata = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata['source_model'],
+                     derived_columns=metadata['derived_columns'],
+                     hashed_columns=metadata['hashed_columns']) }}
