c4b516293372ec9a59f7e82d408949184355bb81
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2026-01-03 18:52:49 +1100

    Update link_customer_account.sql

--- dbt/models/vault_links/link_customer_account.sql
+++ dbt/models/vault_links/link_customer_account.sql
@@ -1,18 +1,40 @@
-{{ config(
-    materialized='table'
-) }}
+-- {{ config(
+--     materialized='table'
+-- ) }}
 
-WITH staged AS (
-    SELECT DISTINCT
-        c.customer_id,
-        a.account_id,
-        CURRENT_TIMESTAMP() AS load_dts,
-        'source_system' AS record_source
-    FROM {{ ref('stg_customer') }} c
-    JOIN {{ source('raw_bronze', 'account') }} a
-        ON c.customer_id = a.customer_id
-)
+-- WITH staged AS (
+--     SELECT DISTINCT
+--         c.customer_id,
+--         a.account_id,
+--         CURRENT_TIMESTAMP() AS load_dts,
+--         'source_system' AS record_source
+--     FROM {{ ref('stg_customer') }} c
+--     JOIN {{ source('raw_bronze', 'account') }} a
+--         ON c.customer_id = a.customer_id
+-- )
 
-SELECT *
-FROM staged
+-- SELECT *
+-- FROM staged
 --- Link tables connect hubs (customer → account) with load metadata.
+
+
+-- dbt/models/vault/links/link_customer_account.sql
+{{ config(materialized='incremental') }}
+
+{%- set yaml_metadata -%}
+source_model: 'stg_accounts'
+src_pk: 'LINK_CUSTOMER_ACCOUNT_HK' -- The unique hash of the relationship
+src_fk:
+  - 'CUSTOMER_HK'                  -- Foreign Key to Hub Customer
+  - 'ACCOUNT_HK'                   -- Foreign Key to Hub Account
+src_ldts: 'LOAD_DATETIME'
+src_source: 'RECORD_SOURCE'
+{%- endset -%}
+
+{% set metadata = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.link(src_pk=metadata['src_pk'],
+                    src_fk=metadata['src_fk'],
+                    src_ldts=metadata['src_ldts'],
+                    src_source=metadata['src_source'],
+                    source_model=metadata['source_model']) }}
