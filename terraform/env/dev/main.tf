8580fa199d7fdce242f84b21c183c44ce82d967d
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2026-01-04 10:50:50 +1100

    Update main.tf

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,11 +1,39 @@
-module "infrastructure" {
-  source = "../../modules" # Pointing up to your shared logic
+terraform {
+  required_version = ">= 1.5.0"
+  
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.92.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-  project_name   = "niloomid-banking-dev"
-  instance_types = [var.eks_instance_type]
-  vpc_id         = var.vpc_id
-  subnet_ids     = var.subnet_ids
+provider "aws" {
+  region = var.aws_region
+}
 
-  # Development Database Settings
-  snowflake_warehouse_size = "XSMALL"
+provider "snowflake" {
+  account  = var.snowflake_account
+  user     = var.snowflake_user
+  password = var.snowflake_password
+  role     = var.snowflake_role
+}
+
+module "s3" {
+  source = "../../modules/s3"
+  
+  environment = var.environment
+  project_name = var.project_name
+}
+
+module "snowflake" {
+  source = "../../modules/snowflake"
+  
+  environment = var.environment
+  database_name = var.snowflake_database
 }
