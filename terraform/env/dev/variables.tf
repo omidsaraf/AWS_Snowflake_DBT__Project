1b7bad4637f41cd6062a0413697925c15d56a4b8
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2026-01-03 21:37:44 +1100

    Update variables.tf

--- terraform/env/dev/variables.tf
+++ terraform/env/dev/variables.tf
@@ -1,12 +1,13 @@
-variable "vpc_id" {
-  default = "vpc-dev-12345"
-}
+variable "project_name" { type = string; default = "niloomid-banking" }
+variable "environment"  { type = string }
+variable "aws_region"   { type = string; default = "ap-southeast-2" }
 
-variable "subnet_ids" {
-  type    = list(string)
-  default = ["subnet-dev-a", "subnet-dev-b"]
-}
+variable "vpc_id"     { type = string }
+variable "subnet_ids" { type = list(string) }
 
-variable "eks_instance_type" {
-  default = "t3.medium" # Cheaper for Dev
-}
+variable "eks_instance_types"       { type = list(string); default = ["t3.medium"] }
+variable "snowflake_warehouse_size" { type = string; default = "XSMALL" }
+
+variable "snowflake_account"  { type = string; sensitive = true }
+variable "snowflake_user"     { type = string; sensitive = true }
+variable "snowflake_password" { type = string; sensitive = true }
